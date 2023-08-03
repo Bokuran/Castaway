cbb63a9e47fc667b1f2d5e37fa230453ef300783
Bokuran <141324043+Bokuran@users.noreply.github.com>
2023-08-03 20:21:45 +0300

    menu-mobile main.js

--- js/main.js
+++ js/main.js
@@ -20,3 +20,10 @@ new Swiper('.image-slider',{
     },
 });
 
+
+const menuBtn = document.querySelector('.menu__btn');
+const menuMobile = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', ()=> {
+    menuMobile.classList.toggle('menu--open');
+});
